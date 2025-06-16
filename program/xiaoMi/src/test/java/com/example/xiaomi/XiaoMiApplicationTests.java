6c563647a873ab347344eb80ca4bb8dd8378dd6b
cug-ptq <2095027453@qq.com>
2025-06-16 20:47:56 +0800

    xiaomi

---
+++ program/xiaoMi/src/test/java/com/example/xiaomi/XiaoMiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xiaomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoMiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
